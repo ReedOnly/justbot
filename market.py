71c7b0997d4bfb232820d3b64be40d178b4d3eb1
ReedOnly <kristian_reed@yahoo.no>
2014-04-09 12:08:32 +0200

    Update market.py

--- market.py
+++ market.py
@@ -2,3 +2,6 @@ from urllib2 import Request, urlopen
 request = Request("https://justcoin.apiary-mock.com/api/v1/markets/{id}/depth")
 response_body = urlopen(request).read()
 print response_body
+
+
+%hei eg er kul
